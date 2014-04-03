84ae40a3577774b735328c928480529b4d84ef27
ramankit <ramankit.singh@gmail.com>
2014-04-04 01:22:09 +0530

    more files added
    index.php enhanced with yamm plugin

--- includes/preconfig.php
+++ includes/preconfig.php
@@ -1,11 +1,22 @@
 <?php
 
-    $bootstrap_path='/bootstrap/bootstrap/css/bootstrap.css';
-    $jquery_path='bootstrap/js/jquery-2.0.3.min.js';
-    $jsplugins='/bootstrap/js/bootstrap.min.js';
+    $base_dir="http://localhost/edaddy/includes";
+   // echo $_SERVER["SERVER_ADDR"];
 
-    require($bootstrap_path);
-    require($jquery_path);
-    require($jsplugins);
+    $bootstrap_path=$base_dir.'/bootstrap/bootstrap/css/bootstrap.css';
+    $jquery_path=$base_dir.'/bootstrap/js/jquery-2.0.3.min.js';
+    $jsplugins=$base_dir.'/bootstrap/js/bootstrap.min.js';
+    $yamm=$base_dir.'/bootstrap/yamm/yamm.css';
+
+    echo '<link rel="stylesheet" href="'.$bootstrap_path.'" >';
+    echo "\n\t";
+
+    echo '<script src="'.$jquery_path.'"></script>';
+    echo "\n\t";
+
+    echo '<script src="'.$jsplugins.'"></script>';
+    echo "\n\t";
+
+    echo '<script src="'.$yamm.'"></script>';
 
 ?>
